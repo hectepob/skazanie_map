965320a7317a205ab784b91f8d7d6cda81555fe4
hectepob <hectepob@rambler.ru>
2026-07-26 21:24:26 +0300

    Update tooltip.js

--- tooltip.js
+++ tooltip.js
@@ -70,19 +70,19 @@ switch (obj.type) {
                 <table class="monsterTooltipTable">
                     <tr>
                         <td class="statName">Урон:</td>
-                        <td colspan="3">-</td>
+                        <td colspan="3">ххх-ххх - ххх-ххх</td>
                     </tr>
                     <tr>
                         <td class="statName">Здоровье:</td>
-                        <td>-</td>
+                        <td>ххх</td>
                         <td class="statName">Броня:</td>
-                        <td>-</td>
+                        <td>ххх</td>
                     </tr>
                     <tr>
                         <td class="statName">Атака:</td>
-                        <td>-</td>
+                        <td>ххх</td>
                         <td class="statName">Защита:</td>
-                        <td>-</td>
+                        <td>ххх</td>
                     </tr>
                  </table>
                  `);
