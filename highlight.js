551e0ed6e3e7e78ce6c02d6e96b0a6b3765919fd
hectepob <hectepob@rambler.ru>
2026-08-03 11:11:40 +0300

    Update highlight.js

--- highlight.js
+++ highlight.js
@@ -5,56 +5,37 @@ const highlight = (function () {
     let render;
 
     function init(obj) {
-
         byId = obj.byId;
         highlightCells = obj.highlightCells;
         render = obj.render;
-
     }
 
     function setCells(ids) {
-
         highlightCells.clear();
-
         ids.forEach(id => {
-
             const cell = byId.get(Number(id));
-
             if (!cell)
                 return;
-
             let root = cell;
-
             while (root.parent_id !== 0) {
-
                 root = byId.get(root.parent_id);
-
                 if (!root)
                     return;
-
             }
-
             highlightCells.add(root.id);
-
         });
-
         renderMap.draw();
-
     }
 
     function clear() {
-
         highlightCells.clear();
         renderMap.draw();
-
     }
 
     return {
-
         init,
         setCells,
         clear
-
     };
 
 })();
